994f5ca08840473f257b845e3009cc344bad2ae9
abhikcshil <abhikshil@ufl.edu>
2023-10-25 20:12:22 -0400

    encode and main functions fixed with comments

--- main.py
+++ main.py
@@ -7,19 +7,20 @@ Description: Learning version control through git
 """
 
 def encode(password):
-    enc_password = ""
-    if len(password) != 8:
+    enc_password = "" #empty output string initiated
+    if len(password) != 8: #ensures password is 8 characters long
         print("password was not 8 characters long")
         exit()
 
-    encoded_list = [int(i) + 3 for i in password]
-    for i in encoded_list: enc_password += str(i)
+    encoding_dict = {0:3, 1:4, 2:5, 3:6, 4:7, 5:8, 6:9, 7:0, 8:1, 9:2}
+    encoded_list = [encoding_dict[int(i)] for i in password] #adds 3 to each integer in password
+    for i in encoded_list: enc_password += str(i) #combines all elements in encoded_list
     print("your encoded password is:", enc_password)
 
 def main():
 
-    password = list(input("Input an 8 digit password containing only integers"))
-    encode(password) #
+    password = list(input("Input an 8 digit password containing only integers")) #user inputs
+    encode(password) #runs encode function on password
 
 
 if __name__ == "__main__":
