db6e044781722ad895cd3ee13e83c0e300260703
abhikcshil <abhikshil@ufl.edu>
2023-10-25 20:21:29 -0400

    encode and main functions fixed with comments (3)

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def encode(password):
     encoding_dict = {0:3, 1:4, 2:5, 3:6, 4:7, 5:8, 6:9, 7:0, 8:1, 9:2}
     encoded_list = [encoding_dict[int(i)] for i in password] #adds 3 to each integer in password
     for i in encoded_list: enc_password += str(i) #combines all elements in encoded_list
-    print("your encoded password is:", enc_password)
+    print("Your encoded password is:", enc_password)
 
 def main():
 
